ced28d15064e99d8910340c7eb024598161e76ef
f-leno <f.leno@usp.br>
2017-04-25 15:15:25 -0300

    Erasing useless files

--- AdHoc/runSimpleExpServerRuben.sh
+++
@@ -1,5 +0,0 @@
-# $1 is the port, $2 the number of repetitions /home/leno/HFO/bin/HFO --offense-agents=3 --defense-npcs=1 --fullstate --headless --trials=33100 --port=$1 --frames-per-trial=200
-for I in $(seq 1 1 $2)
-do
-    /home/ruben/playground/HFO/bin/HFO --no-logging --offense-agents=3 --defense-npcs=1 --fullstate --headless --trials=30100 --port=$1 --frames-per-trial=200
-done
